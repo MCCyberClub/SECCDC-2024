3f119a2b5d3ce22768f8a911851f387d1f577e23
Tim <tim@tim.tim>
2024-02-27 18:12:25 -0800

    improve change_passwords script

--- Linux/change_passwords.py
+++ Linux/change_passwords.py
@@ -3,16 +3,15 @@ import random
 
 from passlib.hash import sha512_crypt
 
-shadow_file = "shadow.txt"
-users = ["root"]
+SHADOW_FILE = "/etc/passwd"
+USERS = ["root"]
+CHAR_SET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^'
+PASSWORD_LENGTH = 15
 
-char_set = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^'
-password_length = 15
-
-def generate_password(password_length, char_set):
+def generate_password():
 	password = ""
-	for i in range(password_length):
-		c = random.choice(char_set)
+	for i in range(PASSWORD_LENGTH):
+		c = random.choice(CHAR_SET)
 		password += c
 	return password
 
@@ -21,8 +20,8 @@ def generate_password_hash(password):
 	hash = sha512_crypt.using(rounds=5000).hash(password)
 	return hash
 
-def change_password(user, shadow_file):
-	with open(shadow_file, "r") as pass_file:
+def change_password(user):
+	with open(SHADOW_FILE, "r") as pass_file:
 		shadow_users = pass_file.readlines()
 
 	new_shadow_contents = ""
@@ -33,7 +32,7 @@ def change_password(user, shadow_file):
 		username = u_splitted[0]
 
 		if username == user:
-			new_password = generate_password(15, char_set)
+			new_password = generate_password(PASSWORD_LENGTH, CHAR_SET)
 			new_password_hash = generate_password_hash(new_password)
 
 			print("[+] Changing password for " + username)
@@ -43,7 +42,10 @@ def change_password(user, shadow_file):
 		
 		new_shadow_contents += ":".join(u_splitted)
 
-	with open(shadow_file, "w") as pass_file:
+	with open(SHADOW_FILE, "w") as pass_file:
 		pass_file.write(new_shadow_contents)
 
-change_password("wazuh", shadow_file)
+
+# EXECUTING IT
+for user in USERS:
+	change_password(user)
